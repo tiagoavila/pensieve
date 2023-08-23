6a6c32393b68c272d46311ad6daba0c8b2f54d09
Tiago Avila <tiago.avila09@gmail.com>
2023-08-22 21:34:20 -0300

    added credo

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :pensieve, Pensieve.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "123456",
   hostname: "localhost",
   database: "pensieve_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
