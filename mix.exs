d84151b773b62c0d09ce9172520631f17a35c678
Tiago Avila <tiago.avila09@gmail.com>
2023-08-27 21:18:47 -0300

    add seed data with Faker

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Pensieve.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:faker, "0.17.0"}
     ]
   end
 
